9c8e9b4f3b316672bce835e8e6da9d28634a3c89
thomasgainant <thomasgainant@gmail.com>
2020-05-11 11:26:03 +0200

    Implemented Unicorns renting business logic (unique active rental & resting time)

--- service/RentalsService.js
+++ service/RentalsService.js
@@ -11,6 +11,17 @@ const sequelize = new Sequelize('unicorn', 'root', '', {
 });
 const Model = Sequelize.Model;
 
+class Unicorn extends Model {}
+Unicorn.init({
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  restingTime: Sequelize.FLOAT //In seconds
+}, { sequelize, modelName: 'unicorn' });
+
 class Rental extends Model {}
 Rental.init({
   id: {
@@ -31,24 +42,117 @@ Rental.init({
   endTime: Sequelize.DATE
 }, { sequelize, modelName: 'rental' });
 
-sequelize.sync().then(() => {});
+sequelize.sync().then(() => {
+  Unicorn.create({
+    id: uuidv4(),
+    name: 'Pinky Pie',
+    restingTime: 15*60
+  })
+  .then(unicorn => {
+    console.log(`Created unicorn ${unicorn.toJSON()}`);
+  });
+
+  Unicorn.create({
+    id: uuidv4(),
+    name: 'Rainbow Dash',
+    restingTime: 15*60
+  })
+  .then(unicorn => {
+    console.log(`Created unicorn ${unicorn.toJSON()}`);
+  });
+
+  Unicorn.create({
+    id: uuidv4(),
+    name: 'Fluttershy',
+    restingTime: 15*60
+  })
+  .then(unicorn => {
+    console.log(`Created unicorn ${unicorn.toJSON()}`);
+  });
+
+  Unicorn.create({
+    id: uuidv4(),
+    name: 'Twilight Sparkle',
+    restingTime: 30*60
+  })
+  .then(unicorn => {
+    console.log(`Created unicorn ${unicorn.toJSON()}`);
+  });
+});
 
 /**
  *
  * body Rental 
  * returns Rental
  **/
-exports.rentUnicorn = function(rentalData) {
+exports.rentUnicorn = function(body) {
   return new Promise(function(resolve, reject) {
-    Rental.create({
-      id: uuidv4(),
-      unicorn: uuidv4(),
-      user: uuidv4(),
-      status: 0,
-      startTime: new Date(),
-      endTime: null
-    }).then(rental => {
-      resolve(rental);
+    Unicorn.findOne({ where: { id: body.unicorn.id } }).then((resUnicorn) => {
+      if(resUnicorn == null){
+        //Unicorn not found
+        reject({payload: "Unicorn not found", code: 404});
+      }
+      else{
+        Rental.findOne({ where: { unicorn: body.unicorn.id, status: 0 } }).then((resRental) => {
+          //If no current Rental for this Unicorn
+          if(resRental == null){
+            Rental.findAll({ where: { unicorn: body.unicorn.id, status: 1 } }).then((rentalsForThisUnicorn) => {
+              let rested = false;
+
+              let lastRental = null;
+              let lastRentalReturnTime = null;
+              for(let rental of rentalsForThisUnicorn){
+                let rentalReturnTime = new Date(rental.endTime);
+
+                if(lastRental == null){
+                  lastRental = rental;
+                  lastRentalReturnTime = rentalReturnTime;
+                }
+                else if(rentalReturnTime > lastRentalReturnTime){
+                    lastRental = rental;
+                    lastRentalReturnTime = rentalReturnTime;
+                }
+              }
+
+              if(lastRental == null){
+                rested = true;
+              }
+              else{
+                var now = new Date();
+                var diff = now.getTime() - lastRentalReturnTime.getTime();
+
+                var secondsBetween = diff / 1000;
+                secondsBetween = Math.abs(secondsBetween);
+
+                if(secondsBetween > resUnicorn.restingTime){
+                  rested = true;
+                }
+              }
+
+              //If unicorn is rested
+              if(rested){
+                Rental.create({
+                  id: uuidv4(),
+                  unicorn: resUnicorn.id,
+                  user: uuidv4(),
+                  status: 0,
+                  startTime: new Date(),
+                  endTime: null
+                }).then(rental => {
+                  resolve(rental);
+                });
+              }
+              else{
+                reject({payload: "This Unicorn is not yet rested", code: 400});
+              }
+            });
+          }
+          else{
+            //This unicorn is rented
+            reject({payload: "Unicorn already rented", code: 404});
+          }
+        });
+      }
     });
   });
 }
@@ -61,18 +165,39 @@ exports.rentUnicorn = function(rentalData) {
  **/
 exports.returnUnicorn = function(body) {
   return new Promise(function(resolve, reject) {
-    Rental.update({
-      unicorn: body.unicorn.id,
-      user: body.user.id,
-      status: 1,
-      startTime: body.startTime,
-      endTime: new Date()
-    }, {
-      where: {
-        id: body.id
+    Unicorn.findOne({ where: { id: body.unicorn.id } }).then((res) => {
+      if(res == null){
+        //Unicorn not found
+        reject({payload: "Unicorn not found", code: 404});
       }
-    }).then((rental) => {
-      resolve(rental)
+      else{
+        Rental.findOne({ where: { unicorn: body.unicorn.id, status: 0 } }).then((resRental) => {
+          //If no current Rental for this Unicorn
+          if(resRental == null){
+            //No Rental not found
+            reject({payload: "Rental not found", code: 404});
+          }
+          else if(resRental.id == body.id){
+            Rental.update({
+              unicorn: resRental.unicorn,
+              user: resRental.user,
+              status: 1,
+              startTime: resRental.startTime,
+              endTime: new Date()
+            }, {
+              where: {
+                id: body.id
+              }
+            }).then((rental) => {
+              resolve(rental)
+            });
+          }
+          else{
+            //Rental with this id not found
+            reject({payload: "Rental not found", code: 404});
+          }
+        });
+      }
     });
   });
 }
